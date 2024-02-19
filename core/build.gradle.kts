8b83cefaf767a69887e17ce09ca08e069a7d0cfd
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-02-19 17:29:52 +0100

    Adds Detekt to check for missing documentation (#11)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,8 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import org.gradle.configurationcache.extensions.capitalized
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.cocoapods)
     alias(libs.plugins.adamko.dokkatoo.html)
+    alias(libs.plugins.arturbosch.detekt)
 }
 
 kotlin {
@@ -25,7 +29,7 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -71,3 +75,44 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
+
+file("src")
+    .listFiles { child -> child.isDirectory }
+    .orEmpty()
+    .also { sourceDirectories ->
+        registerDetektTask(
+            taskName = "detektAll",
+            taskDescription = "Runs Detekt on all source sets.",
+            reportName = "all",
+            sourceDirs = files(sourceDirectories)
+        )
+
+        sourceDirectories.forEach { sourceDir -> registerDetektTask(sourceDir) }
+    }
+
+private fun registerDetektTask(sourceDir: File) =
+    registerDetektTask(sourceDir.name)
+
+private fun registerDetektTask(sourceDir: String) =
+    registerDetektTask(
+        taskName = "detekt${sourceDir.capitalized()}",
+        taskDescription = "Runs Detekt on the $sourceDir source set.",
+        reportName = sourceDir,
+        sourceDirs = files("src/$sourceDir/kotlin")
+    )
+
+private fun registerDetektTask(
+    taskName: String,
+    taskDescription: String,
+    reportName: String,
+    sourceDirs: ConfigurableFileCollection,
+) =
+    tasks.register<Detekt>(taskName) {
+        description = taskDescription
+        setSource(sourceDirs)
+        config = files("$rootDir/config/detekt/detekt.yml")
+        reports {
+            html.outputLocation = file("build/reports/detekt/$reportName.html")
+            xml.outputLocation = file("build/reports/detekt/$reportName.xml")
+        }
+    }
