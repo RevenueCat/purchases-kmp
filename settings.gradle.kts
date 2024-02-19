53d4025a2fe0883e88b1345d4f33678983d27161
JayShortway <5320053-JayShortway@users.noreply.gitlab.com>
2024-02-19 13:57:23 +0100

    Enables FAIL_ON_PROJECT_REPOS.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     repositories {
@@ -8,6 +10,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
         google()
         mavenCentral()
