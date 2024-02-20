82e302db3161a7edca4aeb12c1ac884848b07fdc
JayShortway <5320053-JayShortway@users.noreply.gitlab.com>
2024-02-20 16:15:45 +0100

    Prepares the next development version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 allprojects {
     group = "io.shortway.kobankat"
-    version = "0.1.0" +
+    version = "0.2.0" +
             "-" +
             rootProject.libs.versions.revenuecat.android.get() +
             "-" +
