4d8a159427ad5a181b077bf704a2075d93ea9e5c
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-02-21 14:12:21 +0100

    Adds UpdatedCustomerInfoDelegate (#17)
    Adds UpdatedCustomerInfoDelegate, a convenience implementation of PurchasesDelegate specifically for Android.

--- core/src/androidMain/kotlin/io/shortway/kobankat/PurchasesDelegate.android.kt
+++ core/src/androidMain/kotlin/io/shortway/kobankat/PurchasesDelegate.android.kt
@@ -1,7 +1,27 @@
 package io.shortway.kobankat
 
-import com.revenuecat.purchases.CustomerInfo
 import com.revenuecat.purchases.interfaces.UpdatedCustomerInfoListener
+import io.shortway.kobankat.models.StoreProduct
+import io.shortway.kobankat.models.StoreTransaction
+
+/**
+ * A convenience implementation of [PurchasesDelegate] specifically for Android. Only requires the
+ * caller to provide an implementation of [onCustomerInfoUpdated], just like
+ * `UpdatedCustomerInfoListener` in the official RevenueCat Android SDK.
+ */
+public class UpdatedCustomerInfoDelegate(
+    private val onCustomerInfoUpdated: (customerInfo: CustomerInfo) -> Unit
+) : PurchasesDelegate {
+    override fun onCustomerInfoUpdated(customerInfo: CustomerInfo): Unit =
+        onCustomerInfoUpdated.invoke(customerInfo)
+
+    override fun onPurchasePromoProduct(
+        product: StoreProduct,
+        startPurchase: (onError: (error: PurchasesError, userCancelled: Boolean) -> Unit, onSuccess: (storeTransaction: StoreTransaction, customerInfo: CustomerInfo) -> Unit) -> Unit
+    ) {
+        // No-op on Android.
+    }
+}
 
 internal fun UpdatedCustomerInfoListener.toPurchasesDelegate(): PurchasesDelegate =
     (this as PurchasesDelegateWrapper).wrapped
