a89c9299feb15d2905fe401df80e3d4c88ee15b5
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-02-21 13:32:58 +0100

    Adds default arguments to PurchasesConfiguration (#16)

--- core/src/commonMain/kotlin/io/shortway/kobankat/PurchasesConfiguration.kt
+++ core/src/commonMain/kotlin/io/shortway/kobankat/PurchasesConfiguration.kt
@@ -5,9 +5,9 @@ package io.shortway.kobankat
  */
 public data class PurchasesConfiguration(
     internal val apiKey: String,
-    internal val appUserId: String,
-    internal val observerMode: Boolean,
-    internal val showInAppMessagesAutomatically: Boolean,
-    internal val dangerousSettings: DangerousSettings,
-    internal val verificationMode: EntitlementVerificationMode,
+    internal val appUserId: String? = null,
+    internal val observerMode: Boolean = false,
+    internal val showInAppMessagesAutomatically: Boolean = true,
+    internal val dangerousSettings: DangerousSettings = DangerousSettings(),
+    internal val verificationMode: EntitlementVerificationMode = EntitlementVerificationMode.DISABLED,
 )
