3d60a8af7b89a27c834f230a774551245f2d1b00
JayShortway <5320053-JayShortway@users.noreply.gitlab.com>
2024-02-22 12:19:02 +0100

    Fleshes out the UI a bit.

--- composeApp/src/commonMain/kotlin/io/shortway/kobankat/sample/App.kt
+++ composeApp/src/commonMain/kotlin/io/shortway/kobankat/sample/App.kt
@@ -1,9 +1,15 @@
 package io.shortway.kobankat.sample
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +17,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
 import io.shortway.kobankat.LogLevel
 import io.shortway.kobankat.PurchasesConfiguration
 import io.shortway.kobankat.PurchasesFactory
@@ -31,13 +40,37 @@ fun App() {
 
     MaterialTheme {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                text = "KobanKat logs",
+                modifier = Modifier.padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.h4
+            )
+            Spacer(modifier = Modifier.size(16.dp))
 
-            LazyColumn {
+            val listState = rememberLazyListState()
+            LaunchedEffect(logs.size) {
+                listState.animateScrollToItem(index = logs.lastIndex.coerceAtLeast(0))
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color.LightGray),
+                state = listState,
+                contentPadding = PaddingValues(all = 16.dp)
+            ) {
                 items(logs) {
-                    Text(text = it)
+                    Text(
+                        text = it,
+                        modifier = Modifier.padding(vertical = 4.dp),
+                        fontFamily = FontFamily.Monospace,
+                        style = MaterialTheme.typography.caption
+                    )
                 }
             }
         }
