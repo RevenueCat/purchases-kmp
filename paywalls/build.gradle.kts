5e5115da419e022d4345ba10d3f26b331cfc3763
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-05-31 16:55:29 +0200

    Minimizes dependencies.

--- paywalls/build.gradle.kts
+++ paywalls/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.compose.ExperimentalComposeLibrary
-
 plugins {
     id("kobankat-library")
     alias(libs.plugins.jetbrains.compose)
@@ -12,14 +10,10 @@ kotlin {
             api(projects.core)
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
             implementation(compose.ui)
-            @OptIn(ExperimentalComposeLibrary::class)
-            implementation(compose.components.resources)
         }
         androidMain.dependencies {
             implementation(libs.androidx.compose.ui.tooling.preview)
-            implementation(libs.androidx.activity.compose)
             implementation(libs.revenuecat.commonUi)
         }
     }
@@ -42,11 +36,6 @@ android {
     sourceSets["main"].res.srcDirs("src/androidMain/res")
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
