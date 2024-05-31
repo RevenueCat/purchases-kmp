badf6d7c0d87d5fbc59f7d9c5c9f651e31eb25e5
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-05-31 18:16:23 +0200

    Fixes restoring purchases on Android.

--- paywalls/src/androidMain/kotlin/io/shortway/kobankat/ui/revenuecatui/PaywallOptionsKtx.kt
+++ paywalls/src/androidMain/kotlin/io/shortway/kobankat/ui/revenuecatui/PaywallOptionsKtx.kt
@@ -3,17 +3,21 @@ package io.shortway.kobankat.ui.revenuecatui
 import com.revenuecat.purchases.CustomerInfo
 import com.revenuecat.purchases.ui.revenuecatui.ExperimentalPreviewRevenueCatUIPurchasesAPI
 import com.revenuecat.purchases.ui.revenuecatui.PaywallListener
+import com.revenuecat.purchases.ui.revenuecatui.PaywallOptions as AndroidPaywallOptions
 
 @OptIn(ExperimentalPreviewRevenueCatUIPurchasesAPI::class)
-internal fun PaywallOptions.toRcPaywallOptions(): com.revenuecat.purchases.ui.revenuecatui.PaywallOptions =
-    com.revenuecat.purchases.ui.revenuecatui.PaywallOptions.Builder(dismissRequest)
+internal fun PaywallOptions.toAndroidPaywallOptions(): AndroidPaywallOptions {
+    // Capture this with a different name, to avoid a recursive call in the PaywallListener.
+    val onRestore = onRestoreCompleted
+    return AndroidPaywallOptions.Builder(dismissRequest)
         .setOffering(offering)
         .setListener(
             object : PaywallListener {
                 override fun onRestoreCompleted(customerInfo: CustomerInfo) {
                     super.onRestoreCompleted(customerInfo)
-                    onRestoreCompleted(customerInfo)
+                    onRestore(customerInfo)
                 }
             }
         )
         .build()
+}
