9319477fc5726f969f399ba7e9c647e6b74bb58e
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-06-07 11:41:42 +0200

    Adds docs to PaywallListener to fix Detekt.

--- paywalls/src/commonMain/kotlin/io/shortway/kobankat/ui/revenuecatui/PaywallListener.kt
+++ paywalls/src/commonMain/kotlin/io/shortway/kobankat/ui/revenuecatui/PaywallListener.kt
@@ -5,17 +5,50 @@ import io.shortway.kobankat.Package
 import io.shortway.kobankat.PurchasesError
 import io.shortway.kobankat.models.StoreTransaction
 
+/**
+ * Provide an implementation of this interface to [PaywallOptions] to listen for Paywall events.
+ * Every function has a default no-op implementation.
+ */
 public interface PaywallListener {
+    /**
+     * Callback that gets called when a purchase is started.
+     *
+     * @param rcPackage The package being purchased.
+     */
     public fun onPurchaseStarted(rcPackage: Package) {}
+
+    /**
+     * Callback that gets called when a purchase is completed.
+     */
     public fun onPurchaseCompleted(
         customerInfo: CustomerInfo,
         storeTransaction: StoreTransaction
     ) {
     }
 
+    /**
+     * Callback that gets called when a purchase fails.
+     */
     public fun onPurchaseError(error: PurchasesError) {}
+
+    /**
+     * Callback that gets called when a purchase is cancelled.
+     */
     public fun onPurchaseCancelled() {}
+
+    /**
+     * Callback that gets called when a restore is started.
+     */
     public fun onRestoreStarted() {}
+
+    /**
+     * Callback that gets called when a restore is completed. Note that this may get called even
+     * if no entitlements have been granted in case no relevant purchases were found.
+     */
     public fun onRestoreCompleted(customerInfo: CustomerInfo) {}
+
+    /**
+     * Callback that gets called when a restore fails.
+     */
     public fun onRestoreError(error: PurchasesError) {}
 }
