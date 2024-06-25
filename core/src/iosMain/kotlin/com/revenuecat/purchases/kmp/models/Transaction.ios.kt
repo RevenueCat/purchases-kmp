210c51736edc010aceae3f431fc2c234e77e1f41
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-06-25 22:04:35 +0200

    Uses a dictionary to build a Transaction, instead of the incorrect typealias to RCStoreTransaction.

--- core/src/iosMain/kotlin/com/revenuecat/purchases/kmp/models/Transaction.ios.kt
+++ core/src/iosMain/kotlin/com/revenuecat/purchases/kmp/models/Transaction.ios.kt
@@ -1,13 +1,18 @@
 package com.revenuecat.purchases.kmp.models
 
-import cocoapods.PurchasesHybridCommon.RCStoreTransaction
 import com.revenuecat.purchases.kmp.ktx.toEpochMilliseconds
+import platform.Foundation.NSDate
 
-public actual typealias Transaction = RCStoreTransaction
+// FIXME This should be a typealias of RCNonSubscriptionTransaction so we don't need to use a map.
+public actual class Transaction internal constructor(map: Map<String, Any?>) {
+    internal val transactionIdentifier: String by map
+    internal val productIdentifier: String by map
+    internal val purchaseDate: NSDate by map
+}
 
 public actual val Transaction.transactionIdentifier: String
-    get() = transactionIdentifier()
+    get() = transactionIdentifier
 public actual val Transaction.productIdentifier: String
-    get() = productIdentifier()
+    get() = productIdentifier
 public actual val Transaction.purchaseDateMillis: Long
-    get() = purchaseDate().toEpochMilliseconds()
+    get() = purchaseDate.toEpochMilliseconds()
