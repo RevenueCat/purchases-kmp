17bb55882c4a09dcebae42a889514c556a3f142e
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-06-25 14:14:27 +0200

    Downgrades Kotlin to 1.9.23. Updates Compose Multiplatform to 1.6.11.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.jetbrains.compose).apply(false)
-    alias(libs.plugins.compose.compiler).apply(false)
     alias(libs.plugins.kotlin.multiplatform).apply(false)
     alias(libs.plugins.kotlin.cocoapods).apply(false)
     alias(libs.plugins.kotlinx.binaryCompatibilityValidator)
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.compose)
-    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
