ac6b3172d06eac57c9725aeda10124c372172c0f
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-07-08 17:02:36 +0200

    Removes the Compose Compiler Gradle Plugin from :apiTester. (#125)

--- apiTester/build.gradle.kts
+++ apiTester/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.compose)
-    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
