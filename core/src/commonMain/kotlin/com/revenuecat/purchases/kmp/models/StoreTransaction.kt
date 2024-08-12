525300e19ef4591e242e8db87d44ec101e93ddb3
Will Taylor <wtaylor151@gmail.com>
2024-08-12 13:04:41 -0500

    create storetransaction class

--- core/src/commonMain/kotlin/com/revenuecat/purchases/kmp/models/StoreTransaction.kt
+++ core/src/commonMain/kotlin/com/revenuecat/purchases/kmp/models/StoreTransaction.kt
@@ -3,24 +3,80 @@ package com.revenuecat.purchases.kmp.models
 /**
  * Represents an in-app billing purchase.
  */
-public expect class StoreTransaction
+public class StoreTransaction(
 
-/**
- * Unique (per store) order identifier for the purchased transaction. Always null for Amazon.
- * Only available for non-restored Google purchases for Google Play.
- */
-public expect val StoreTransaction.transactionId: String?
+    /**
+     * Unique (per store) order identifier for the purchased transaction. Always null for Amazon.
+     * Only available for non-restored Google purchases for Google Play.
+     */
+    public val transactionId: String,
 
-/**
- * Product IDs purchased.
- *
- * Size can be greater than 1 on Android. This indicates that a multi-line purchase occurred,
- * which RevenueCat does not support.
- * Only the first productId will be processed by the SDK.
- */
-public expect val StoreTransaction.productIds: List<String>
+    /**
+     * Product IDs purchased.
+     *
+     * Size can be greater than 1 on Android. This indicates that a multi-line purchase occurred,
+     * which RevenueCat does not support.
+     * Only the first productId will be processed by the SDK.
+     */
+    public val productIDs: List<String>,
 
-/**
- * Time the product was purchased, in milliseconds since the epoch.
- */
-public expect val StoreTransaction.purchaseTime: Long
+    /**
+     * Time the product was purchased, in milliseconds since the epoch.
+     */
+    public val purchaseTime: Long
+) {
+    public constructor(map: Map<String, Any>) : this(
+        transactionId = map["transactionIdentifier"] as? String
+            ?: throw IllegalArgumentException("transactionIdentifier is missing or not a String"),
+        productIDs = map["productIdentifier"]?.let { listOf(it as String) }
+            ?: throw IllegalArgumentException("productIdentifier is missing or not a String"),
+        purchaseTime = map["purchaseDateMillis"] as? Long
+            ?: throw IllegalArgumentException("purchaseDateMillis is missing or not a Long")
+    )
+}
+
+///**
+// * Represents an in-app billing purchase.
+// */
+//public expect class StoreTransaction {
+//
+//    /**
+//     * Unique (per store) order identifier for the purchased transaction. Always null for Amazon.
+//     * Only available for non-restored Google purchases for Google Play.
+//     */
+//    public val transactionId: String
+//
+//    /**
+//     * Product IDs purchased.
+//     *
+//     * Size can be greater than 1 on Android. This indicates that a multi-line purchase occurred,
+//     * which RevenueCat does not support.
+//     * Only the first productId will be processed by the SDK.
+//     */
+//    public val productIDs: List<String>
+//
+//    /**
+//     * Time the product was purchased, in milliseconds since the epoch.
+//     */
+//    public val purchaseTime: Long
+//}
+
+///**
+// * Unique (per store) order identifier for the purchased transaction. Always null for Amazon.
+// * Only available for non-restored Google purchases for Google Play.
+// */
+//public expect val StoreTransaction.transactionId: String?
+//
+///**
+// * Product IDs purchased.
+// *
+// * Size can be greater than 1 on Android. This indicates that a multi-line purchase occurred,
+// * which RevenueCat does not support.
+// * Only the first productId will be processed by the SDK.
+// */
+//public expect val StoreTransaction.productIds: List<String>
+//
+///**
+// * Time the product was purchased, in milliseconds since the epoch.
+// */
+//public expect val StoreTransaction.purchaseTime: Long
--- core/src/iosMain/kotlin/com/revenuecat/purchases/kmp/models/StoreTransaction.ios.kt
+++ core/src/iosMain/kotlin/com/revenuecat/purchases/kmp/models/StoreTransaction.ios.kt
@@ -1,15 +1,17 @@
 package com.revenuecat.purchases.kmp.models
-
-import cocoapods.PurchasesHybridCommon.RCStoreTransaction
-import com.revenuecat.purchases.kmp.ktx.toEpochMilliseconds
-
-public actual typealias StoreTransaction = RCStoreTransaction
-
-public actual val StoreTransaction.transactionId: String?
-    get() = transactionIdentifier()
-
-public actual val StoreTransaction.productIds: List<String>
-    get() = listOf(productIdentifier())
-
-public actual val StoreTransaction.purchaseTime: Long
-    get() = purchaseDate().toEpochMilliseconds()
+//
+//public actual class StoreTransaction(
+//    public actual val transactionId: String,
+//    public actual val productIDs: List<String>,
+//    public actual val purchaseTime: Long
+//) {
+//    public constructor(map: Map<String, Any>) : this(
+//        transactionId = map["transactionIdentifier"] as? String
+//            ?: throw IllegalArgumentException("transactionIdentifier is missing or not a String"),
+//        productIDs = map["productIdentifier"]?.let { listOf(it as String) }
+//            ?: throw IllegalArgumentException("productIdentifier is missing or not a String"),
+//        purchaseTime = map["purchaseDateMillis"] as? Long
+//            ?: throw IllegalArgumentException("purchaseDateMillis is missing or not a Long")
+//    )
+//}
+//
