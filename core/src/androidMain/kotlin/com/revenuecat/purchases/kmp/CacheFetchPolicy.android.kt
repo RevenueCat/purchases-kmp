fc107dc5948f5ec1773121bd7216d4ab03a6d748
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-08-13 09:32:43 +0200

    Reenables #137 (#143)

--- core/src/androidMain/kotlin/com/revenuecat/purchases/kmp/CacheFetchPolicy.android.kt
+++ core/src/androidMain/kotlin/com/revenuecat/purchases/kmp/CacheFetchPolicy.android.kt
@@ -1,5 +1,11 @@
 package com.revenuecat.purchases.kmp
 
-import com.revenuecat.purchases.CacheFetchPolicy as RcCacheFetchPolicy
+import com.revenuecat.purchases.CacheFetchPolicy as AndroidCacheFetchPolicy
 
-public actual typealias CacheFetchPolicy = RcCacheFetchPolicy
+internal fun CacheFetchPolicy.toAndroidCacheFetchPolicy(): AndroidCacheFetchPolicy =
+    when (this) {
+        CacheFetchPolicy.CACHE_ONLY -> AndroidCacheFetchPolicy.CACHE_ONLY
+        CacheFetchPolicy.FETCH_CURRENT -> AndroidCacheFetchPolicy.FETCH_CURRENT
+        CacheFetchPolicy.NOT_STALE_CACHED_OR_CURRENT -> AndroidCacheFetchPolicy.NOT_STALE_CACHED_OR_CURRENT
+        CacheFetchPolicy.CACHED_OR_FETCHED -> AndroidCacheFetchPolicy.CACHED_OR_FETCHED
+    }
--- core/src/androidMain/kotlin/com/revenuecat/purchases/kmp/models/DiscountType.android.kt
+++
@@ -1,6 +0,0 @@
-package com.revenuecat.purchases.kmp.models
-
-public actual enum class DiscountType {
-    INTRODUCTORY,
-    PROMOTIONAL,
-}
