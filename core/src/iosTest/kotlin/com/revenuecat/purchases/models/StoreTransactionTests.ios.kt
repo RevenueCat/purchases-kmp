ebf20826b8752302347e192b98c9961bb3250797
Will Taylor <wtaylor151@gmail.com>
2024-08-16 13:38:36 -0500

    add int test

--- core/src/iosTest/kotlin/com/revenuecat/purchases/models/StoreTransactionTests.ios.kt
+++ core/src/iosTest/kotlin/com/revenuecat/purchases/models/StoreTransactionTests.ios.kt
@@ -35,6 +35,31 @@ class StoreTransactionTests {
     }
 
     @Test
+    fun `fromMap correctly parses map with all fields if purchaseDateMillis is an int`() {
+        val transactionId = "someTransactionIdentifier"
+        val productId = "someProductIdentifier"
+        val purchaseDateMillis = 1627689600000
+        val purchaseDate = "2021-07-31T00:00:00Z"
+
+        val hybridMap: Map<Any?, *> = mapOf(
+            "transactionIdentifier" to transactionId,
+            "revenueCatId" to transactionId,    // Deprecated
+            "productIdentifier" to productId,
+            "productId" to productId,   // Deprecated
+            "purchaseDateMillis" to purchaseDateMillis,
+            "purchaseDate" to purchaseDate
+        )
+
+        val result = StoreTransaction.fromMap(storeTransactionMap = hybridMap)
+
+        assertTrue(result.isSuccess)
+        val storeTransaction = result.getOrThrow()
+        assertEquals(transactionId, storeTransaction.transactionId)
+        assertEquals(listOf(productId), storeTransaction.productIds)
+        assertEquals(purchaseDateMillis.toLong(), storeTransaction.purchaseTime)
+    }
+
+    @Test
     fun `missing transactionIdentifier returns failure`() {
         val productId = "someProductIdentifier"
         val purchaseDateMillis = 1627689600000.0
