d7b09d5c24c285aa9894f61fcbf43a30a3ed7d18
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-08-22 18:37:06 +0200

    Adds some dummy code.

--- mappings/src/iosMain/kotlin/com/revenuecat/purchases/kmp/mappings/CustomerInfo.ios.kt
+++ mappings/src/iosMain/kotlin/com/revenuecat/purchases/kmp/mappings/CustomerInfo.ios.kt
@@ -1,1 +1,6 @@
 package com.revenuecat.purchases.kmp.mappings
+
+import cocoapods.PurchasesHybridCommon.RCCustomerInfo as IosCustomerInfo
+
+// This file is only created so the directory structure is properly set up on git.
+// TODO Add some actual code here.
