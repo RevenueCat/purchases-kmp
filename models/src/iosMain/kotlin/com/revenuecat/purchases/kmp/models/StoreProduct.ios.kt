ce5c612266dd29c216257973285bbd07dfc5f072
JayShortway <29483617+JayShortway@users.noreply.github.com>
2024-08-23 16:41:50 +0200

    Comments out the dummy StoreProduct.

--- models/src/iosMain/kotlin/com/revenuecat/purchases/kmp/models/StoreProduct.ios.kt
+++ models/src/iosMain/kotlin/com/revenuecat/purchases/kmp/models/StoreProduct.ios.kt
@@ -2,7 +2,7 @@ package com.revenuecat.purchases.kmp.models
 
 import cocoapods.PurchasesHybridCommon.RCStoreProduct as IosStoreProduct
 
-public actual class StoreProduct
-
 // This file is only created so the directory structure is properly set up on git.
 // TODO Add some actual code here.
+
+// public actual class StoreProduct
