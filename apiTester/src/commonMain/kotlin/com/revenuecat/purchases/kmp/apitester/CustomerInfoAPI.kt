be086811b14cb0e333f093cca534f5aef1617aba
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-09-29 14:16:37 +0200

    Moves CustomerInfo Instant extension properties to member fields.

--- apiTester/src/commonMain/kotlin/com/revenuecat/purchases/kmp/apitester/CustomerInfoAPI.kt
+++ apiTester/src/commonMain/kotlin/com/revenuecat/purchases/kmp/apitester/CustomerInfoAPI.kt
@@ -11,29 +11,36 @@ import com.revenuecat.purchases.kmp.datetime.requestInstant
 import com.revenuecat.purchases.kmp.models.CustomerInfo
 import com.revenuecat.purchases.kmp.models.EntitlementInfos
 import com.revenuecat.purchases.kmp.models.Transaction
-import kotlinx.datetime.Instant
+import kotlin.time.ExperimentalTime
 
 private class CustomerInfoAPI {
+    @OptIn(ExperimentalTime::class)
     fun check(customerInfo: CustomerInfo) {
         with(customerInfo) {
             val entitlementInfo: EntitlementInfos = entitlements
             val asubs = activeSubscriptions
             val productIds: Set<String> = allPurchasedProductIdentifiers
-            val led: Long? = latestExpirationDateMillis
-            val lei: Instant? = latestExpirationInstant
+            val ledm: Long? = latestExpirationDateMillis
+            val lei: kotlinx.datetime.Instant? = latestExpirationInstant
+            val led: kotlin.time.Instant? = latestExpirationDate
             val nst: List<Transaction> = nonSubscriptionTransactions
-            val opd: Long? = originalPurchaseDateMillis
-            val opi: Instant? = originalPurchaseInstant
-            val rd: Long = requestDateMillis
-            val ri: Instant = requestInstant
+            val opdm: Long? = originalPurchaseDateMillis
+            val opi: kotlinx.datetime.Instant? = originalPurchaseInstant
+            val opd: kotlin.time.Instant? = originalPurchaseDate
+            val rdm: Long = requestDateMillis
+            val ri: kotlinx.datetime.Instant = requestInstant
+            val rd: kotlin.time.Instant = requestDate
             val fsm: Long = firstSeenMillis
-            val fsi: Instant = firstSeenInstant
+            val fsi: kotlinx.datetime.Instant = firstSeenInstant
+            val fs: kotlin.time.Instant = firstSeen
             val oaui: String = originalAppUserId
             val mu: String? = managementUrlString
             val allExpirationMillisByProduct: Map<String, Long?> = allExpirationDateMillis
-            val allExpirationInstantsByProduct: Map<String, Instant?> = allExpirationInstants
+            val allExpirationInstantsByProduct: Map<String, kotlinx.datetime.Instant?> = allExpirationInstants
+            val allExpirationDatesByProduct: Map<String, kotlin.time.Instant?> = allExpirationDates
             val allPurchaseMillisByProduct: Map<String, Long?> = allPurchaseDateMillis
-            val allPurchaseInstantsByProduct: Map<String, Instant?> = allPurchaseInstants
+            val allPurchaseInstantsByProduct: Map<String, kotlinx.datetime.Instant?> = allPurchaseInstants
+            val allPurchaseDatesByProduct: Map<String, kotlin.time.Instant?> = allPurchaseDates
         }
     }
 }
