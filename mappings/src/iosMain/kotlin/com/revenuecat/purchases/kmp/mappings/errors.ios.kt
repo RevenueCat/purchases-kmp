635c20a1fe72dcc485d35634ea6a83400641646b
Antonio Pallares <antonio.pallares@revenuecat.com>
2025-10-10 12:51:04 +0200

    Add missing errors coming from iOS

--- mappings/src/iosMain/kotlin/com/revenuecat/purchases/kmp/mappings/errors.ios.kt
+++ mappings/src/iosMain/kotlin/com/revenuecat/purchases/kmp/mappings/errors.ios.kt
@@ -50,6 +50,10 @@ private fun Long.toPurchasesErrorCode(): PurchasesErrorCode =
         35L -> PurchasesErrorCode.OfflineConnectionError
         36L -> PurchasesErrorCode.FeatureNotAvailableInCustomEntitlementsComputationMode
         37L -> PurchasesErrorCode.SignatureVerificationError
+        38L -> PurchasesErrorCode.FeatureNotSupportedWithStoreKit1
+        39L -> PurchasesErrorCode.InvalidWebPurchaseToken
+        40L -> PurchasesErrorCode.PurchaseBelongsToOtherUser
+        41L -> PurchasesErrorCode.ExpiredWebPurchaseToken
         42L -> PurchasesErrorCode.TestStoreSimulatedPurchaseError
         else -> error("Unexpected ErrorCode: $this")
 }
