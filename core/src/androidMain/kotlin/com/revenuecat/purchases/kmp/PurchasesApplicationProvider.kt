a2dedc23374c2ea24a2fad81d278a645fce444d6
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-11-03 16:11:16 +0100

    Fixes docs.

--- core/src/androidMain/kotlin/com/revenuecat/purchases/kmp/PurchasesApplicationProvider.kt
+++ core/src/androidMain/kotlin/com/revenuecat/purchases/kmp/PurchasesApplicationProvider.kt
@@ -6,13 +6,13 @@ import androidx.startup.Initializer
 import com.revenuecat.purchases.kmp.di.AndroidProvider
 
 /**
- * This class is only used to capture the [android.app.Application] instance and keep a reference to it. The
+ * This class is only used to capture the [Application] instance and keep a reference to it. The
  * `Application` instance is used for 2 things:
  * 1. To call static Purchases methods that need a Context, such as
  * [canMakePayments][com.revenuecat.purchases.Purchases.canMakePayments].
  * 2. To keep track of the current Activity, which is needed to make purchases.
  *
- * If you need to use RevenueCat in your own [androidx.startup.Initializer], make sure to add
+ * If you need to use RevenueCat in your own [Initializer], make sure to add
  * [PurchasesApplicationProvider] to its dependencies like so:
  *
  * ```kotlin
