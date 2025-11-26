3cf3361c20d0b2a8cd56d5b1f35f39ac2e355a05
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-11-26 22:23:50 +0100

    Adds new modules for the SPM dependencies

---
+++ kn-core/src/iosMain/kotlin/com/revenuecat/purchases/kn/core/Dummy.kt
@@ -0,0 +1,7 @@
+package com.revenuecat.purchases.kn.core
+
+/**
+ * A dummy class to make sure this module's artifact gets compiled by the compileKotlinIos* tasks
+ * and the proper .klib (in build/classes/kotlin/<target>/main/klib/) gets created.
+ */
+private object Dummy
---
+++ kn-ui/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("revenuecat-public-library")
+}
+
+kotlin {
+    swiftPMDependencies {
+        `package`(
+            url = url("https://github.com/RevenueCat/purchases-hybrid-common.git"),
+            version = exact(libs.versions.revenuecat.common.get()),
+            products = listOf(product("PurchasesHybridCommonUI")),
+        )
+    }
+}
+
+android {
+    namespace = "com.revenuecat.purchases.kn.ui.revenuecatui"
+}
