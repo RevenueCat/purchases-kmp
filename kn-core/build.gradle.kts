d250f70c313c60b81a347781f6763ce9d729391a
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-12-18 11:46:15 +0100

    Uses swiftPackage in the facade modules

--- kn-core/build.gradle.kts
+++ kn-core/build.gradle.kts
@@ -1,5 +1,19 @@
+import com.revenuecat.purchases.kmp.buildlogic.swift.swiftPackage
+
 plugins {
     id("revenuecat-library")
+}
+
+kotlin {
+    sourceSets {
+        iosMain.dependencies {
+            swiftPackage(
+                path = rootProject.file("upstream/purchases-ios"),
+                target = "RevenueCat",
+                packageName = "swiftPMImport.com.revenuecat.purchases.kn.core"
+            )
+        }
+    }
 }
 
 android {
--- kn-ui/build.gradle.kts
+++ kn-ui/build.gradle.kts
@@ -1,5 +1,19 @@
+import com.revenuecat.purchases.kmp.buildlogic.swift.swiftPackage
+
 plugins {
     id("revenuecat-library")
+}
+
+kotlin {
+    sourceSets {
+        iosMain.dependencies {
+            swiftPackage(
+                path = rootProject.file("upstream/purchases-ios"),
+                target = "RevenueCatUI",
+                packageName = "swiftPMImport.com.revenuecat.purchases.kn.ui"
+            )
+        }
+    }
 }
 
 android {
