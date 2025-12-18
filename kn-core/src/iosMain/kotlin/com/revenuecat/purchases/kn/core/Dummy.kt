b4a3b8b731dc214e6c35a874f9674c63a3c1d4c9
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-12-18 11:46:15 +0100

    Uses swiftPackage in the facade modules

---
+++ kn-core/src/iosMain/kotlin/com/revenuecat/purchases/kn/core/Dummy.kt
@@ -0,0 +1,8 @@
+@file:Suppress("unused")
+
+package com.revenuecat.purchases.kn.core
+
+/**
+ * We need at least 1 Kotlin source file for the kn-core module to be published.
+ */
+internal object Dummy
