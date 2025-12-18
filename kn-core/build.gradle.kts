d14a576dd671b7dfbd850408645a510f237be2fa
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-12-18 11:46:15 +0100

    Uses swiftPackage in the facade modules

--- kn-core/build.gradle.kts
+++ kn-core/build.gradle.kts
@@ -1,5 +1,25 @@
+import com.revenuecat.purchases.kmp.buildlogic.swift.swiftPackage
+
 plugins {
     id("revenuecat-library")
+}
+
+kotlin {
+    sourceSets {
+        iosMain.dependencies {
+            swiftPackage(
+                path = rootProject.file("upstream/purchases-ios"),
+                target = "RevenueCat",
+                packageName = "swiftPMImport.com.revenuecat.purchases.kn.core",
+                customDeclarations = """
+                    // Force cinterop binding generation for types otherwise not in the public API
+                    static inline int __forceBindings(
+                        enum RCStoreMessageType _1
+                    ) { return 0; }
+                """.trimIndent()
+            )
+        }
+    }
 }
 
 android {
