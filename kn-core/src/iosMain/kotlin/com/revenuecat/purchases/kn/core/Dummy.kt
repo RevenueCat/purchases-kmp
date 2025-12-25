f677fcb087420c1d61a0da9f4350ef9210099745
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-12-25 14:41:26 +0100

    Adds Dummy.kt files. Might not be needed.

---
+++ kn-core/src/iosMain/kotlin/com/revenuecat/purchases/kn/core/Dummy.kt
@@ -0,0 +1,5 @@
+@file:Suppress("unused")
+
+package com.revenuecat.purchases.kn.core
+
+internal object Dummy
---
+++ kn-ui/src/iosMain/kotlin/com/revenuecat/purchases/kn/ui/Dummy.kt
@@ -0,0 +1,5 @@
+@file:Suppress("unused")
+
+package com.revenuecat.purchases.kn.ui
+
+internal object Dummy
