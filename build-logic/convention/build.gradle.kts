b668a9469cd054b6e57e6cc62b0da9ba8ecd570e
JayShortway <29483617+JayShortway@users.noreply.github.com>
2025-12-30 21:49:14 +0100

    Adds support for Swift resources

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -18,6 +18,8 @@ tasks.withType<KotlinCompile>().configureEach {
 
 dependencies {
     implementation(libs.android.gradlePlugin)
+    implementation(libs.compose.gradlePlugin)
+    implementation(libs.composeCompiler.gradlePlugin)
     implementation(libs.detekt.gradlePlugin)
     implementation(libs.dokkatoo.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
