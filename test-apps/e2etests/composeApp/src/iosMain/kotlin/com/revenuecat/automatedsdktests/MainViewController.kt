757a49ca2f0179f92fb906cc002eda5aa8786921
JayShortway <29483617+JayShortway@users.noreply.github.com>
2026-02-26 10:21:19 +0100

    Adds maestro setup

---
+++ test-apps/e2etests/composeApp/src/iosMain/kotlin/com/revenuecat/automatedsdktests/MainViewController.kt
@@ -0,0 +1,6 @@
+package com.revenuecat.automatedsdktests
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+@Suppress("unused", "FunctionName")
+fun MainViewController(offeringId: String?) = ComposeUIViewController { App(offeringId) }
