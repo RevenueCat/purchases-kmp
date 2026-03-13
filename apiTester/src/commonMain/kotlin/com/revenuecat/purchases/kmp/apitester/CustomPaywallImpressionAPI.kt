3380b84a1d5ba6fb3819fa0d1e0f82c8b51ba016
Rick van der Linden <rick.vanderlinden@revenuecat.com>
2026-03-13 09:45:59 +0100

    Add API tester for trackCustomPaywallImpression
    Update CustomPaywallImpressionAPI to use the new simplified paywallId
    parameter and add checks to PurchasesCommonAPI.

--- apiTester/src/commonMain/kotlin/com/revenuecat/purchases/kmp/apitester/CustomPaywallImpressionAPI.kt
+++ apiTester/src/commonMain/kotlin/com/revenuecat/purchases/kmp/apitester/CustomPaywallImpressionAPI.kt
@@ -1,26 +1,13 @@
 package com.revenuecat.purchases.kmp.apitester
 
-import com.revenuecat.purchases.kmp.ExperimentalRevenueCatApi
 import com.revenuecat.purchases.kmp.Purchases
-import com.revenuecat.purchases.kmp.models.CustomPaywallImpressionParams
 
 @Suppress("unused", "UNUSED_VARIABLE")
-@OptIn(ExperimentalRevenueCatApi::class)
 private class CustomPaywallImpressionAPI {
     fun check(purchases: Purchases) {
-        // CustomPaywallImpressionParams constructors
-        val defaultParams: CustomPaywallImpressionParams = CustomPaywallImpressionParams()
-        val paramsWithId: CustomPaywallImpressionParams =
-            CustomPaywallImpressionParams(paywallId = "my-paywall")
-        val paramsWithNull: CustomPaywallImpressionParams =
-            CustomPaywallImpressionParams(paywallId = null)
-
-        // CustomPaywallImpressionParams properties
-        val paywallId: String? = paramsWithId.paywallId
-
         // trackCustomPaywallImpression API
         purchases.trackCustomPaywallImpression()
-        purchases.trackCustomPaywallImpression(defaultParams)
-        purchases.trackCustomPaywallImpression(paramsWithId)
+        purchases.trackCustomPaywallImpression(paywallId = null)
+        purchases.trackCustomPaywallImpression(paywallId = "my-paywall")
     }
 }
