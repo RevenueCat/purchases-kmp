528b80c3865f7c729ece6d5c600b31478f54d11e
JayShortway <29483617+JayShortway@users.noreply.github.com>
2026-04-01 22:09:42 +0200

    Removes dead code (#779)

--- mappings/src/iosMain/kotlin/com/revenuecat/purchases/kmp/mappings/StoreTransaction.ios.kt
+++ mappings/src/iosMain/kotlin/com/revenuecat/purchases/kmp/mappings/StoreTransaction.ios.kt
@@ -10,29 +10,3 @@ public fun IosStoreTransaction.toStoreTransaction(): StoreTransaction =
         productIds = listOf(productIdentifier()),
         purchaseTime = purchaseDate().toEpochMilliseconds(),
     )
-
-public fun buildStoreTransaction(storeTransactionMap: Map<Any?, *>): Result<StoreTransaction> {
-    val transactionId = storeTransactionMap["transactionIdentifier"] as? String
-    val productId = storeTransactionMap["productIdentifier"] as? String
-    val purchaseTime = (storeTransactionMap["purchaseDateMillis"] as? Number)?.toLong()
-
-    if(transactionId == null) {
-        return Result.failure(IllegalArgumentException("Expected a non-null transactionIdentifier"))
-    }
-
-    if(productId == null) {
-        return Result.failure(IllegalArgumentException("Expected a non-null productIdentifier"))
-    }
-
-    if(purchaseTime == null) {
-        return Result.failure(IllegalArgumentException("Expected a non-null purchaseDateMillis"))
-    }
-
-    return Result.success(
-        StoreTransaction(
-            transactionId = transactionId,
-            productIds = listOf(productId),
-            purchaseTime = purchaseTime
-        )
-    )
-}
