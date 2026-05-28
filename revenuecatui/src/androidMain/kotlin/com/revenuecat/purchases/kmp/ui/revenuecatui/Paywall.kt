66c819c1b7b034eb9e3aeaa50007bbedddc415df
Ranbir Singh <poetdroid2@gmail.com>
2026-05-28 13:47:02 +0530

    Fix Paywall crash when Purchases is not configured

--- revenuecatui/src/androidMain/kotlin/com/revenuecat/purchases/kmp/ui/revenuecatui/Paywall.kt
+++ revenuecatui/src/androidMain/kotlin/com/revenuecat/purchases/kmp/ui/revenuecatui/Paywall.kt
@@ -1,9 +1,28 @@
 package com.revenuecat.purchases.kmp.ui.revenuecatui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.revenuecat.purchases.kmp.Purchases
+import com.revenuecat.purchases.kmp.models.PurchasesError
+import com.revenuecat.purchases.kmp.models.PurchasesErrorCode
 import com.revenuecat.purchases.ui.revenuecatui.ExperimentalPreviewRevenueCatUIPurchasesAPI
 import com.revenuecat.purchases.ui.revenuecatui.Paywall as RcPaywall
 
 @Composable
-public actual fun Paywall(options: PaywallOptions): Unit =
+public actual fun Paywall(options: PaywallOptions): Unit {
+    if (!Purchases.isConfigured) {
+        LaunchedEffect(options) {
+            options.listener?.onPurchaseError(
+                PurchasesError(
+                    code = PurchasesErrorCode.ConfigurationError,
+                    underlyingErrorMessage = "Purchases is not configured. " +
+                        "Call Purchases.configure(...) before showing Paywall."
+                )
+            )
+            options.dismissRequest()
+        }
+        return
+    }
+
     RcPaywall(options.toAndroidPaywallOptions())
+}
