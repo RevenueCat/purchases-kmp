6a78912233e5a63f9399aacfc79312c871a77462
Peter Porfy <peter.porfy@revenuecat.com>
2026-08-17 18:11:24 +0200

    chore(ads): align ad tracking class visibility

--- models/src/commonMain/kotlin/com/revenuecat/purchases/kmp/models/AdMediatorName.kt
+++ models/src/commonMain/kotlin/com/revenuecat/purchases/kmp/models/AdMediatorName.kt
@@ -1,14 +1,23 @@
 package com.revenuecat.purchases.kmp.models
 
 import com.revenuecat.purchases.kmp.ExperimentalRevenueCatApi
+import com.revenuecat.purchases.kmp.InternalRevenueCatApi
 
 /**
  * Common ad mediator names.
  */
 @ExperimentalRevenueCatApi
-public class AdMediatorName(public val value: String) {
+public class AdMediatorName internal constructor(@property:InternalRevenueCatApi public val value: String) {
     public companion object {
         public val AD_MOB: AdMediatorName = AdMediatorName("AdMob")
         public val APP_LOVIN: AdMediatorName = AdMediatorName("AppLovin")
+
+        public fun fromString(value: String): AdMediatorName {
+            return when (value.trim()) {
+                "AdMob" -> AD_MOB
+                "AppLovin" -> APP_LOVIN
+                else -> AdMediatorName(value)
+            }
+        }
     }
 }
--- models/src/commonTest/kotlin/com/revenuecat/purchases/kmp/models/AdEventTypesTest.kt
+++ models/src/commonTest/kotlin/com/revenuecat/purchases/kmp/models/AdEventTypesTest.kt
@@ -1,11 +1,12 @@
 package com.revenuecat.purchases.kmp.models
 
 import com.revenuecat.purchases.kmp.ExperimentalRevenueCatApi
+import com.revenuecat.purchases.kmp.InternalRevenueCatApi
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
-@OptIn(ExperimentalRevenueCatApi::class)
+@OptIn(ExperimentalRevenueCatApi::class, InternalRevenueCatApi::class)
 class AdEventTypesTest {
 
     @Test
