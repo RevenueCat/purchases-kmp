4c5e9dd6884d4c1f0af3758394613c4bcc80e5c3
Peter Porfy <peter.porfy@revenuecat.com>
2026-08-18 14:36:43 +0200

    chore(ads): add comment for internal annotation

--- models/src/commonMain/kotlin/com/revenuecat/purchases/kmp/InternalRevenueCatApi.kt
+++ models/src/commonMain/kotlin/com/revenuecat/purchases/kmp/InternalRevenueCatApi.kt
@@ -1,5 +1,11 @@
 package com.revenuecat.purchases.kmp
 
+/**
+ * This annotation marks RevenueCat APIs that are internal and not meant for public consumption.
+ *
+ * APIs marked with this annotation may change frequently and without warning, and are not
+ * covered by semantic versioning guarantees. Do not use in your own code.
+ */
 @Retention(value = AnnotationRetention.BINARY)
 @RequiresOptIn(
     level = RequiresOptIn.Level.ERROR,
