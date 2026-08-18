8d006d2e514217ecb8e924d22337a8b0af9b1969
Peter Porfy <peter.porfy@revenuecat.com>
2026-08-18 14:29:50 +0200

    fix(ads): add equals and haschode

--- models/src/commonMain/kotlin/com/revenuecat/purchases/kmp/models/AdMediatorName.kt
+++ models/src/commonMain/kotlin/com/revenuecat/purchases/kmp/models/AdMediatorName.kt
@@ -8,6 +8,18 @@ import com.revenuecat.purchases.kmp.InternalRevenueCatApi
  */
 @ExperimentalRevenueCatApi
 public class AdMediatorName internal constructor(@property:InternalRevenueCatApi public val value: String) {
+    @OptIn(InternalRevenueCatApi::class)
+    public override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is AdMediatorName) return false
+        return value == other.value
+    }
+
+    @OptIn(InternalRevenueCatApi::class)
+    public override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
     public companion object {
         public val AD_MOB: AdMediatorName = AdMediatorName("AdMob")
         public val APP_LOVIN: AdMediatorName = AdMediatorName("AppLovin")
