efd8e59d57f75b5df47714bfb8a2a4fcef211a68
Peter Porfy <peter.porfy@revenuecat.com>
2026-08-18 21:03:30 +0200

    fix(ads): fix api and add tests

---
+++ apiTester/src/commonMain/kotlin/com/revenuecat/purchases/kmp/apitester/RewardedAdTrackingMetadataAPI.kt
@@ -0,0 +1,32 @@
+package com.revenuecat.purchases.kmp.apitester
+
+import com.revenuecat.purchases.kmp.ExperimentalRevenueCatApi
+import com.revenuecat.purchases.kmp.models.AdFormat
+import com.revenuecat.purchases.kmp.models.AdMediatorName
+import com.revenuecat.purchases.kmp.models.RewardedAdTrackingMetadata
+
+@Suppress("unused", "UNUSED_VARIABLE")
+private class RewardedAdTrackingMetadataAPI {
+    @OptIn(ExperimentalRevenueCatApi::class)
+    fun check(metadata: RewardedAdTrackingMetadata) {
+        val networkName: String? = metadata.networkName
+        val mediatorName: AdMediatorName = metadata.mediatorName
+        val adFormat: AdFormat = metadata.adFormat
+        val placement: String? = metadata.placement
+        val adUnitId: String = metadata.adUnitId
+        val impressionId: String = metadata.impressionId
+    }
+
+    @OptIn(ExperimentalRevenueCatApi::class)
+    fun checkConstructor() {
+        val metadata = RewardedAdTrackingMetadata(
+            networkName = "network-name",
+            mediatorName = AdMediatorName.AD_MOB,
+            adFormat = AdFormat.REWARDED,
+            placement = "placement",
+            adUnitId = "ad-unit-id",
+            impressionId = "impression-id",
+        )
+        check(metadata)
+    }
+}
